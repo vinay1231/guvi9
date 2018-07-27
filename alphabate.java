e37dc0bc598d0d1e61b096064c5110ee01195373
vinay1231 <41731317+vinay1231@users.noreply.github.com>
2018-07-27 12:34:45 +0530

    Update alphabate.java

--- alphabate.java
+++ alphabate.java
@@ -12,7 +12,7 @@ class Ideone
 	 ch=sc.next().charAt(0);
 	 if(ch=='q'||ch=='w'||ch=='r'||ch=='e'||ch=='t'||ch=='y'||ch=='u'||ch=='i'||ch=='o'||ch=='p'||ch=='a'||ch=='s'||ch=='d'||ch=='f'||ch=='g'||ch=='h'||ch=='j'||ch=='k'||ch=='k'||ch=='l'||ch=='z'||ch=='x'||ch=='c'||ch=='v'||ch=='b'||ch=='n'||ch=='m')
 	 {
-	 	System.out.println("Alphabate");
+	 	System.out.println("Alphabet");
 	 }
 	 else
 	 {
